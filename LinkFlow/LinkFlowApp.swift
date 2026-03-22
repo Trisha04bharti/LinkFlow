203e7b436138c695d5314778883d6eeb1acc4596
Trisha Bharti <trishabharti444@gmail.com>
2026-03-22 12:38:57 +0530

    Initial Commit

---
+++ LinkFlow/LinkFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkFlowApp.swift
+//  LinkFlow
+//
+//  Created by Vikram Kumar on 22/03/26.
+//
+
+import SwiftUI
+
+@main
+struct LinkFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
