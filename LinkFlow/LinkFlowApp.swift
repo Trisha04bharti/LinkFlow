9655db6026a57f7ebaeaf7b4b70d877e01d5dfae
Trisha Bharti <trishabharti444@gmail.com>
2026-04-01 21:47:24 +0530

    Add AppItem model and sample app list data

--- LinkFlow/LinkFlowApp.swift
+++ LinkFlow/LinkFlowApp.swift
@@ -6,15 +6,26 @@
 //
 
 import SwiftUI
-import Firebase
+import FirebaseCore   // ✅ important
+
+// ✅ Step 1: AppDelegate
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        
+        FirebaseApp.configure()   // ✅ Firebase starts here
+        
+        return true
+    }
+}
 
 @main
 struct LinkFlowApp: App {
     
-    init() {
-          FirebaseApp.configure()   // ✅ ADD THIS
-      }
-    
+    // ✅ Step 2: connect AppDelegate
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
